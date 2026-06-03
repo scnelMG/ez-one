78abede03fe867992e7180a8485b6998904e1c26
scnelMG <qkralsrb4407@naver.com>
2026-06-04 00:29:19 +0900

    refactor(auth): use current Mockito test annotation

--- backend/src/test/java/com/ezone/backend/controller/AuthControllerTest.java
+++ backend/src/test/java/com/ezone/backend/controller/AuthControllerTest.java
@@ -13,9 +13,9 @@ import com.ezone.backend.service.AuthService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AuthController.class)
@@ -25,7 +25,7 @@ class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AuthService authService;
 
     @Test
